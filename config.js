ddc61cd225009a0f1d6f99bc8e0b2c25dd2fc397
Kanyango <kariukikanyango@gmail.com>
2018-02-07 18:11:45 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 exports.mongodb = {
-	uri: "mongodb://127.0.0.1:27017/zezeDB"
+	uri : "mongodb://baraka:baraka123@ds125938.mlab.com:25938/zezedb"
+	//uri: "mongodb://127.0.0.1:27017/zezeDB"
 	//uri: 'mongodb://kariuki:androidapps@ds033126.mlab.com:33126/smsappdb'
 };
 exports.oauth = {
