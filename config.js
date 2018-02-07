68709848f59f3c98e786d20aabf452014cc91a4c
Kanyango <kariukikanyango@gmail.com>
2018-02-07 18:07:35 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,3 +2,24 @@ exports.mongodb = {
 	uri: "mongodb://127.0.0.1:27017/zezeDB"
 	//uri: 'mongodb://kariuki:androidapps@ds033126.mlab.com:33126/smsappdb'
 };
+exports.oauth = {
+
+	'facebook' :{
+
+		'clientID'    : '1091247900936084',
+		'clientSecret': '941a60d3455544c0aa1ffbae17d3d95d',
+		'callbackURL' : 'http://localhost:7000/oauth/facebook/callback'
+	},
+
+	'twitter' :{
+		'consumerKey'    : 't3r87nEjaUpQpyayIzRwKPhOO',
+		'consumerSecret' : 'OI5xaXTgkURzEKkIMbONQuYudAJTOq0mve509Vl39lW3iUFwrD',
+		'callbackUrl' : 'http://localhost:7000/oauth/twitter/callback'
+	},
+
+	'cloudinary' :{
+		'cloud_name'    : 'dxomvhu0p',
+		'api_key'       : '811296612498678',
+		'api_secret'    : 'j8BV1pcR-Jagxi63jCJSAMrImVM'
+	}
+};
