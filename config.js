5e98ee6bfb710778111f5a46d948baa51bd3b6d9
Kanyango <kariukikanyango@gmail.com>
2018-05-09 12:44:42 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,8 @@ exports.mongodb = {
 	//uri: "mongodb://127.0.0.1:27017/zezeDB"
 	//uri: 'mongodb://kariuki:androidapps@ds033126.mlab.com:33126/smsappdb'
 };
+exports.secret = 'b7TY?>m6wl_i/<';
+
 exports.oauth = {
 
 	'facebook' :{
